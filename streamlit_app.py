72354575f859cd4331fefc2179eac5296a450b3d
nagachi <rafly.rigannagachi@gmail.com>
2021-11-16 10:07:58 +0700

    text based wm

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,57 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+from PIL import Image, ImageDraw, ImageColor, ImageFont
+import io
 
-"""
-# Welcome to Streamlit!
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# set sidebar for input
+upload_file = st.sidebar.file_uploader("Upload image: ")
+user_input = st.sidebar.text_area(
+    "Watermark text: ", "Watermark text\ngoes here")
+font_size = st.sidebar.slider("Font size: ", 1, 100, 32)
+font_style = st.sidebar.selectbox(
+    'Choose font: ', ('arial.ttf', 'arial.ttf'))
+color_hex = st.sidebar.color_picker("Background color hex: ", "#eee")
+alph = st.sidebar.slider("Opacity: ", 0, 255, 200)
 
+# set font
+font = ImageFont.truetype(font_style, size=font_size)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# sample image path
+dir = './wm-maker/Lenna_(test_image).png'
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+# check uploaded file
+if upload_file:
+    dir = upload_file
 
-    points_per_turn = total_points / num_turns
+image_data = Image.open(dir)
+size = image_data.size  # (width, height)
+st.text('Shape: '+str(size))
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+# set sidebar for axis input
+x_axis = st.sidebar.slider("X: ", -200, size[0], size[0]//4)
+y_axis = st.sidebar.slider("Y: ", -200, size[1], size[1]//2)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+col1, col2 = st.columns(2)
+
+# initial image
+col1.subheader('Canvas')
+col1.image(image_data, width=100, use_column_width=True)
+
+# draw text to image
+color_rgba = (*ImageColor.getcolor(color_hex, 'RGB'), alph)
+txt = Image.new('RGBA', size, (255, 255, 255, 0))
+draw = ImageDraw.Draw(txt)
+draw.text((x_axis, y_axis), user_input, color_rgba, font=font)
+image_data.paste(txt, (0, 0), txt)
+
+# prepare image for download
+img_byte = io.BytesIO()
+image_data.save(img_byte, format='PNG')
+img_byte = img_byte.getvalue()
+
+# add draw image to col2
+col2.subheader('Result')
+col2.image(image_data, width=100, use_column_width=True, output_format='PNG')
+
+# download image
+st.download_button('Download file', data=img_byte,
+                   file_name='result.png', mime='image/png')
